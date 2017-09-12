056209d2d2601f9710acd99f2e68abc1de994e44
Riccellisp <riccellisp456@gmail.com>
2017-09-12 09:40:31 -0300

    maybe a internationalization

---
+++ public_html/js/index.js
@@ -0,0 +1,27 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+var x = "User-agent header sent: " + navigator.userAgent;
+strArch1 = new String("32");
+strArch2 = new String("64");
+if (x.search(strArch2) !== -1) {
+    document.getElementById("bt2").className = "btn btn-lg btn-outline";
+    document.getElementById("bt1").className = "btn btn-lg btn-outline-info";
+}
+
+if (x.search(strArch1) !== -1) {
+    document.getElementById("bt1").className = "btn btn-lg btn-outline";
+    document.getElementById("bt2").className = "btn btn-lg btn-outline-info";
+}
+
+function downloadClick32(){
+    window.alert("32");
+}
+
+function downloadClick64(){
+    window.alert("64");
+}
+
+
